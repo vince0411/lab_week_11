aac1ef67cf543d259508c0419f75343f62d6491c
Vince Enriquez <0411vince@gmail.com>
2021-03-26 23:05:33 -0700

    Updated code for p5 to ensure instructions are followed properly and also used destructuring in p6 to get todo value

--- JS/app.js
+++ JS/app.js
@@ -4,11 +4,22 @@ const todoList = { todo: "Finish Assignment 1 by March 19", status: "complete" }
 completedTodos = []
 // []
 
-// p5: Create for loops to check if todoList objects status is complete //
-for (status in todoList) { if (status == status) {completedTodos.push(status)}}
-// 2
+
+// p5: Create a for loop to check if todoList objects status is complete //
+for (status in todoList) { 
+    if (status == "status") 
+    {completedTodos.push(todoList.status)}}
+// 1
 console.log(completedTodos)
-// ["todo", "status"]
+// ["complete"]
+
+
+// p6: Create a second for loop to log todo value onto console using destructuring//
+for (todo in completedTodos)   {
+    const value = todoList.todo
+    console.log(value)
+}
+// Finish Assignment 1 by March 19
 
 
 
